df0b7fdad59a433b0de582ec0e73df1810d335c0
ravigordon <rgordz460@outlook.com>
2022-05-10 12:30:25 +0100

    function and logic created to calculate actual weekley hours slept

---
+++ Desktop/sleepDebtCalculator/app.js
@@ -0,0 +1,35 @@
+const getSleepHours = day =>{
+    if(day === 'monday'){
+      return 9;
+    }
+    else if(day === 'tuesday'){
+      return 9;
+    }
+    else if(day === 'wednesday'){
+      return 9;
+    }
+    else if(day === 'thursday'){
+      return 9;
+    }
+    else if(day === 'friday'){
+      return 9;
+    }
+    else if(day === 'saturday'){
+      return 9;
+    }
+    else if(day === 'sunday'){
+      return 9;
+    }
+    else{
+      return "Error!"
+    }
+  };
+  getActualSleepHours = () =>{
+    return getSleepHours('monday')+
+     getSleepHours('tuesday')+
+     getSleepHours('wednesday')+
+     getSleepHours('thursday')+
+     getSleepHours('friday')+
+     getSleepHours('saturday')+
+     getSleepHours('sunday')
+   }
